3d46da4d48ebd6422305bf8b0d96837c02663c78
Susan Cheng <susan.doggie@gmail.com>
2025-07-01 13:39:43 +0800

    clean

---
+++ Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version: 6.0
+//
+//  Package.swift
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2025 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftJS",
+    platforms: [
+        .macOS(.v14),
+        .macCatalyst(.v17),
+        .iOS(.v17),
+    ],
+    products: [
+        .executable(name: "SwiftJSTests", targets: ["SwiftJSTests"]),
+        .library(name: "SwiftJS", targets: ["SwiftJS"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.2"),
+    ],
+    targets: [
+        .target(
+            name: "SwiftJS",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto")
+            ],
+            resources: [
+                .copy("resources/polyfill.js")
+            ]
+        ),
+        .executableTarget(
+            name: "SwiftJSTests",
+            dependencies: ["SwiftJS"],
+            resources: [
+                .copy("resources/corejs.js"),
+                .copy("resources/script_1.js"),
+                .copy("resources/script_2.js")
+            ]
+        ),
+    ]
+)
---
+++ Sources/SwiftJS/core/core.swift
@@ -0,0 +1,76 @@
+//
+//  core.swift
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2025 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import JavaScriptCore
+
+public struct SwiftJS {
+    
+    public let virtualMachine: VirtualMachine
+    
+    let base: JSContext
+    let context = Context()
+    
+    public init(_ virtualMachine: VirtualMachine = VirtualMachine()) {
+        self.virtualMachine = virtualMachine
+        self.base = JSContext(virtualMachine: virtualMachine.base)
+        self.base.exceptionHandler = { context, exception in
+            guard let message = exception?.toString() else { return }
+            print(message)
+        }
+        self.polyfill()
+    }
+}
+
+extension SwiftJS: @unchecked Sendable {}
+
+extension SwiftJS {
+    
+    public var runloop: RunLoop {
+        return self.virtualMachine.runloop
+    }
+}
+
+extension SwiftJS {
+    
+    public var globalObject: SwiftJS.Value {
+        return SwiftJS.Value(self.base.globalObject)
+    }
+    
+    public var exception: SwiftJS.Value {
+        return self.base.exception.map(SwiftJS.Value.init) ?? .undefined
+    }
+    
+    @discardableResult
+    public func evaluateScript(_ script: String) -> SwiftJS.Value {
+        let result = self.base.evaluateScript(script)
+        return result.map(SwiftJS.Value.init) ?? .undefined
+    }
+    
+    @discardableResult
+    public func evaluateScript(_ script: String, withSourceURL sourceURL: URL) -> SwiftJS.Value {
+        let result = self.base.evaluateScript(script, withSourceURL: sourceURL)
+        return result.map(SwiftJS.Value.init) ?? .undefined
+    }
+}
